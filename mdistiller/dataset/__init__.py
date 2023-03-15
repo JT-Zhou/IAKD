45218407e8282a879f90dd5b3b51a24fccefb84f
JT-Zhou <127947797+JT-Zhou@users.noreply.github.com>
2023-03-15 20:12:26 +0800

    Add files via upload

---
+++ mdistiller/dataset/__init__.py
@@ -0,0 +1,65 @@
+from .cifar100 import get_cifar100_dataloaders, get_cifar100_dataloaders_sample
+from .imagenet import get_imagenet_dataloaders, get_imagenet_dataloaders_sample
+from .cifar10 import get_cifar10_dataloaders,get_cifar10_dataloaders_sample
+from .flower import get_F_dataloaders
+def get_dataset(cfg):
+    if cfg.DATASET.TYPE == "cifar100":
+        if cfg.DISTILLER.TYPE == "CRD":
+            train_loader, val_loader, num_data = get_cifar100_dataloaders_sample(
+                batch_size=cfg.SOLVER.BATCH_SIZE,
+                val_batch_size=cfg.DATASET.TEST.BATCH_SIZE,
+                num_workers=cfg.DATASET.NUM_WORKERS,
+                k=cfg.CRD.NCE.K,
+                mode=cfg.CRD.MODE,
+            )
+        else:
+            train_loader, val_loader, num_data = get_cifar100_dataloaders(
+                batch_size=cfg.SOLVER.BATCH_SIZE,
+                val_batch_size=cfg.DATASET.TEST.BATCH_SIZE,
+                num_workers=cfg.DATASET.NUM_WORKERS,
+            )
+        num_classes = 100
+    elif cfg.DATASET.TYPE == "imagenet":
+        if cfg.DISTILLER.TYPE == "CRD":
+            train_loader, val_loader, num_data = get_imagenet_dataloaders_sample(
+                batch_size=cfg.SOLVER.BATCH_SIZE,
+                val_batch_size=cfg.DATASET.TEST.BATCH_SIZE,
+                num_workers=cfg.DATASET.NUM_WORKERS,
+                k=cfg.CRD.NCE.K,
+            )
+        else:
+            train_loader, val_loader, num_data = get_imagenet_dataloaders(
+                batch_size=cfg.SOLVER.BATCH_SIZE,
+                val_batch_size=cfg.DATASET.TEST.BATCH_SIZE,
+                num_workers=cfg.DATASET.NUM_WORKERS,
+            )
+        num_classes = 1000
+    elif cfg.DATASET.TYPE == "cifar10":
+        if cfg.DISTILLER.TYPE == "CRD":
+            train_loader, val_loader, num_data = get_cifar10_dataloaders_sample(
+                batch_size=cfg.SOLVER.BATCH_SIZE,
+                val_batch_size=cfg.DATASET.TEST.BATCH_SIZE,
+                num_workers=cfg.DATASET.NUM_WORKERS,
+                k=cfg.CRD.NCE.K,
+                mode=cfg.CRD.MODE,
+            )
+        else:
+            train_loader, val_loader, num_data = get_cifar10_dataloaders(
+                batch_size=cfg.SOLVER.BATCH_SIZE,
+                val_batch_size=cfg.DATASET.TEST.BATCH_SIZE,
+                num_workers=cfg.DATASET.NUM_WORKERS,
+            )
+        num_classes = 10
+
+    else:
+        raise NotImplementedError(cfg.DATASET.TYPE)
+
+    return train_loader, val_loader, num_data, num_classes
+
+if __name__ =="__main__":
+    train_loader, val_loader, num_data = get_cub_dataloaders(
+        batch_size=32,
+        val_batch_size=4,
+        num_workers=1,
+    )
+    num_classes = 200
